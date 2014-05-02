f178a85820b19bc61d80e2ae7d887a38a1ea690a
Ethan <ethan@callisto.local>
2014-05-01 21:33:52 -0700

    Implemented '.' functionality for regexp parser

--- regexpADP.py
+++ regexpADP.py
@@ -4,5 +4,21 @@ class RegexpADP():
     def __init__(self, pattern):
         self.pattern = pattern
 
-    def match(self, string):
-        return
+    def match(self, testString):
+        retval = []
+        # we can start anywhere in our comparison string (at least until we
+        # implement ^) so we'll make a separate pass starting at each
+        # position in our test string
+        for i, c in enumerate(testString):
+            # want to be sure our whole regexp fits
+            candidate = ''
+            for j, d in enumerate(self.pattern):
+                if(i+j < len(testString) and (d == testString[i+j] or d == '.')):
+                    candidate += testString[i+j]
+                    continue
+                else:
+                    candidate = ''
+                    break
+            if(len(candidate)):
+                retval.append(candidate)
+        return retval
--- regexpTests.py
+++ regexpTests.py
@@ -5,14 +5,22 @@ from regexpADP import RegexpADP
 class TestRegexpADPFunctions(unittest.TestCase):
     """Test cases for the RegepADP class"""
 
-    def test_match(self):
+    def test_match_dot_one(self):
         regexp = RegexpADP('hello.world')
         self.assertEqual(regexp.match("hello|world"), ["hello|world"])
+
+    def test_match_dot_two(self):
+        regexp = RegexpADP('hello.world')
         self.assertEqual(regexp.match(
             "hello-world helloworld hello,world hello world"),
             ["hello-world", "hello,world", "hello world"])
-        self.assertTrue(True)
-        # self.assertTrue(False)
+
+    def test_match_question(self):
+        regexp = RegexpADP('hello?world')
+        # self.assertEqual(regexp.match("hello"), ["hello"])
+        # self.assertEqual(regexp.match("hell"), ["hell"])
+        # self.assertEqual(regexp.match("helloworld"), ["helloworld"])
+        # self.assertEqual(regexp.match("hellworld"), ["hellworld"])
 
 if __name__ == '__main__':
     unittest.main()
