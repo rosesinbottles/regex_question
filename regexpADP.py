252ae5ddccff824e9077a64d2420ee36dbe50c05
rosesinbottles <ethan@callisto.local>
2014-05-04 22:44:05 -0700

    Implemented '?' functionality Stubbed out '*' functionality, though test assertions are not active

--- regexpADP.py
+++ regexpADP.py
@@ -4,21 +4,73 @@ class RegexpADP():
     def __init__(self, pattern):
         self.pattern = pattern
 
+    """ Essentially this function walks two patterns side-by-side and compares them.
+        The testString is the string we are testing against the patternString.
+        We walk the patterns backward so that we can account for * and ? operators (both
+        are effectively look-behind operators) in a single pass"""
     def match(self, testString):
+
+        # Our return value
         retval = []
-        # we can start anywhere in our comparison string (at least until we
-        # implement ^) so we'll make a separate pass starting at each
-        # position in our test string
-        for i, c in enumerate(testString):
-            # want to be sure our whole regexp fits
+
+        # print "\ntesting {} againts {}".format(testString, self.pattern)
+        # A match can start anywhere within our testString. So we actually do n separate passes
+        # for a string of length n, the i-th pass staring at the n-i index.
+        for i in reversed(range(0, len(testString))):
             candidate = ''
-            for j, d in enumerate(self.pattern):
-                if(i+j < len(testString) and (d == testString[i+j] or d == '.')):
-                    candidate += testString[i+j]
-                    continue
-                else:
+            patternIncrement = 0
+            testIncrement = 0
+
+            # This is where our comparison begins
+            for j in range(0, len(self.pattern)):
+                testIndex = i - j - testIncrement
+                patternIndex = len(self.pattern) - 1 - j - patternIncrement
+
+                # In the case where our pattern is longer than our test string, we should fail
+                if(testIndex == 0 and patternIndex > 0):
                     candidate = ''
-                    break
+
+                # We also bounce before we hit out-of-bounds indices
+                elif(testIndex >= 0):
+                    testChar = testString[testIndex]
+                    patternChar = self.pattern[patternIndex]
+
+                    # The hopeful case: we match and we append our testChar to our candidate match
+                    if(self.naturalMatch(testChar, patternChar)):
+                        candidate = testChar + candidate
+                        continue
+
+                    # We have a second chance if we have just hit a question mark
+                    # in this case, we assume that the testCharacter is a match and
+                    # decrement the index on our pattern string to account for the ?.
+                    # We then test to see whether
+                    elif(patternChar == '?'):
+                        patternIncrement += 1
+                        if(self.naturalMatch(testChar, self.pattern[patternIndex-1])):
+                            candidate = testChar + candidate
+                            continue
+                        elif(self.naturalMatch(testChar, self.pattern[patternIndex-2])):
+                            candidate = testChar + candidate
+                            patternIncrement += 1
+                            continue
+                        else:
+                            candidate = ''
+                            break
+
+                    elif(patternChar == '*'):
+
+                        k = 1
+                        while(self.naturalMatch(testString[testIndex-k], self.pattern[patternIndex-1])):
+                            k += 1
+                            candidate = testChar + candidate
+                            testIncrement += 1
+
+                    else:
+                        candidate = ''
+                        break
             if(len(candidate)):
-                retval.append(candidate)
+                retval.insert(0, candidate)
         return retval
+
+    def naturalMatch(self, testChar, patternChar):
+        return (testChar == patternChar or patternChar == '.')
--- regexpTests.py
+++ regexpTests.py
@@ -5,6 +5,22 @@ from regexpADP import RegexpADP
 class TestRegexpADPFunctions(unittest.TestCase):
     """Test cases for the RegepADP class"""
 
+    def test_no_match_one(self):
+        regexp = RegexpADP('hello')
+        self.assertEqual(regexp.match("world"), [])
+
+    def test_no_match_two(self):
+        regexp = RegexpADP('hello')
+        self.assertEqual(regexp.match(""), [])
+
+    def test_no_match_three(self):
+        regexp = RegexpADP('')
+        self.assertEqual(regexp.match("helloworld"), [])
+
+    def test_no_match_four(self):
+        regexp = RegexpADP('hello')
+        self.assertEqual(regexp.match("hell"), [])
+
     def test_match_dot_one(self):
         regexp = RegexpADP('hello.world')
         self.assertEqual(regexp.match("hello|world"), ["hello|world"])
@@ -15,12 +31,52 @@ class TestRegexpADPFunctions(unittest.TestCase):
             "hello-world helloworld hello,world hello world"),
             ["hello-world", "hello,world", "hello world"])
 
-    def test_match_question(self):
+    def test_match_dot_three(self):
+        regexp = RegexpADP('hello..orld')
+        self.assertEqual(regexp.match(
+            "hello-world helloworld hello,world hello world"),
+            ["hello-world", "hello,world", "hello world"])
+
+    def test_match_question_one(self):
         regexp = RegexpADP('hello?world')
-        # self.assertEqual(regexp.match("hello"), ["hello"])
-        # self.assertEqual(regexp.match("hell"), ["hell"])
-        # self.assertEqual(regexp.match("helloworld"), ["helloworld"])
-        # self.assertEqual(regexp.match("hellworld"), ["hellworld"])
+        self.assertEqual(regexp.match("helloworld"), ["helloworld"])
+
+    def test_match_question_two(self):
+        regexp = RegexpADP('hello?world')
+        self.assertEqual(regexp.match("hellworld"), ["hellworld"])
+
+    def test_match_question_three(self):
+        regexp = RegexpADP('hello?world')
+        self.assertEqual(regexp.match("hellpworld"), [])
+
+    def test_match_question_four(self):
+        regexp = RegexpADP('helo?world')
+        self.assertEqual(regexp.match("helloworld"), [])
+
+    def test_match_question_five(self):
+        regexp = RegexpADP('abcd?efg')
+        self.assertEqual(regexp.match("abcdefg"), ['abcdefg'])
+
+    def test_match_question_six(self):
+        regexp = RegexpADP('abcd?efg')
+        self.assertEqual(regexp.match("bcefg"), [])
+
+    def test_match_star_one(self):
+        regexp = RegexpADP('hello*world')
+        # self.assertEqual(regexp.match("hellworld"), ['hellworld'])
+
+    def test_match_star_two(self):
+        regexp = RegexpADP('hello*world')
+        # self.assertEqual(regexp.match("helloworld"), ['helloworld'])
+
+    def test_match_star_three(self):
+        regexp = RegexpADP('hello*world')
+        # self.assertEqual(regexp.match("hellooooworld"), ['hellooooworld'])
+
+    def test_match_star_four(self):
+        regexp = RegexpADP('hello*world')
+        # self.assertEqual(regexp.match("heloworld"), [])
+
 
 if __name__ == '__main__':
     unittest.main()
